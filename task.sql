ac588bd66cf2d2005001855abc6a75c6dbfb5b2f
Marko Panchak <marko.panchak1@gmail.com>
2024-06-18 14:19:41 +0300

    Solution

--- task.sql
+++ task.sql
@@ -9,15 +9,29 @@ CREATE TABLE Countries (
     PRIMARY KEY (ID)
 );
 
+CREATE TABLE Products (
+    ID INT,
+	  ProductName VARCHAR(50),
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE Warehouses (
+	  ID INT,
+    WarehouseName VARCHAR(50),
+    WarehouseAddress VARCHAR(50),
+	  CountryID INT,
+	  FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
+    PRIMARY KEY (ID)
+);
+
 CREATE TABLE ProductInventory (
     ID INT,
-    ProductName VARCHAR(50),
+	  ProductID INT,
+    FOREIGN KEY (ProductID) REFERENCES Products(ID) ON DELETE NO ACTION,
+    WarehouseID INT,
+    FOREIGN KEY (WarehouseID) REFERENCES Warehouses(ID) ON DELETE NO ACTION,
     WarehouseAmount INT,
-    WarehouseName VARCHAR(50),
-    WarehouseAddress VARCHAR(50), 
-    CountryID INT,
-	FOREIGN KEY (CountryID) REFERENCES Countries(ID) ON DELETE NO ACTION,
-    PRIMARY KEY (ID)
+	  PRIMARY KEY (ID)
 );
 
 -- Populate test data
@@ -26,8 +40,14 @@ INSERT INTO Countries (ID,Name)
 	VALUES (1, 'Country1');
 INSERT INTO Countries (ID,Name)
 	VALUES (2, 'Country2');
-    
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (1, 'AwersomeProduct', 2, 'Warehouse-1', 'City-1, Street-1',1);
-INSERT INTO ProductInventory (ID,ProductName,WarehouseAmount,WarehouseName,WarehouseAddress,CountryID)
-	VALUES (2, 'AwersomeProduct', 5, 'Warehouse-2', 'City-2, Street-2',2);
+INSERT INTO Products (ID,ProductName)
+	VALUES (1, 'Product');
+INSERT INTO Warehouses (ID,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (1, 'Kiev1','Khreshchatyk1','1');
+INSERT INTO Warehouses (ID,WarehouseName,WarehouseAddress,CountryID)
+	VALUES (2, 'Kiev2','Khreshchatyk2','2');
+INSERT INTO ProductInventory (ID,ProductID,WarehouseID,WarehouseAmount)
+	VALUES (1, 1, 1, 4);
+INSERT INTO ProductInventory (ID,ProductID,WarehouseID,WarehouseAmount)
+	VALUES (2, 1, 2, 5);
+
